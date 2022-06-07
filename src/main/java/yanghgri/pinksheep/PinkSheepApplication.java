a9383aa18bc4d2dd7ca667226d0f94f0f176253e
YangHgRi <1987758983@qq.com>
2022-06-07 14:47:54 +0800

    V0.01

---
+++ src/main/java/yanghgri/pinksheep/PinkSheepApplication.java
@@ -0,0 +1,13 @@
+package yanghgri.pinksheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkSheepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkSheepApplication.class, args);
+    }
+
+}
